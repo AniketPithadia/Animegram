30fc2df95f20374c171d59f1931902099ecc1d7d
Aniket <aniket@pop-os.localdomain>
2023-01-05 17:03:41 +0530

    files added

---
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -0,0 +1,37 @@
+import { Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+
+const AdvertWidget = () => {
+  const { palette } = useTheme();
+  const dark = palette.neutral.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
+
+  return (
+    <WidgetWrapper>
+      <FlexBetween>
+        <Typography color={dark} variant="h5" fontWeight="500">
+          Sponsored
+        </Typography>
+        <Typography color={medium}>Buy Anime Merch</Typography>
+      </FlexBetween>
+      <img
+        width="100%"
+        height="auto"
+        alt="advert"
+        src="http://localhost:3001/assets/info4.jpeg"
+        style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+      />
+      <FlexBetween>
+        <Typography color={main}>Anime Clothing</Typography>
+        <Typography color={medium}>animeclothing.com</Typography>
+      </FlexBetween>
+      <Typography color={medium} m="0.5rem 0">
+        Your pathway to looking stunnig and powerful just like your anime idols.
+      </Typography>
+    </WidgetWrapper>
+  );
+};
+
+export default AdvertWidget;
